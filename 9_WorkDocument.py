6b0fea73ef67c2d4de0becaf24bdd51f8bedef6d
SNOWNUMB <mamayev.stas@gmail.com>
2025-02-16 21:29:31 +0500

    Данные добавлены 16.02.2025

--- 9_WorkDocument.py
+++ 9_WorkDocument.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class Payable(ABC):
     @abstractmethod
-    def pay(self, amount):
+    def pey(self, amount):
         pass
 
 class CreditCard(Payable):
@@ -18,10 +18,10 @@ class CreditCard(Payable):
     def limit(self):
         return self.__limit
 
-    def pay(self, amount):
-        if self.__balance + self.__limit >= amount:
-            self.__balance -= amount
-            print(f"Оплачено {amount}. Новый баланс: {self.__balance}")
+    def pey(self, anount):
+        if self.__balance + self.__limit >= anount:
+            self.__balance -= anount
+            print(f"Оплачено {anount}. Новый баланс: {self.__balance}")
         else:
             print("Недостаточно средств (включая кредитный лимит).")
 
@@ -44,7 +44,7 @@ class Person:
         if value >= 18:
             self.__age = value
         else:
-            print("Возраст не может быть меньше 18 лет.")
+            print("Возраст не может быть меньше 18 лет")
 
     def introduce(self):
         return f"Меня зовут {self.__name}, мне {self.__age} лет."
@@ -54,25 +54,24 @@ class Student(Person, Payable):
         super().__init__(name, age, credit_card)
         self.university = university
 
-    def pay(self, amount):
-        discounted_amount = amount * 0.9  # 10% скидка
-        self.credit_card.pay(discounted_amount)
+    def pey(self, amount):
+        discounted_amount = amount * 0.9
+        self.credit_card.pey(discounted_amount)
 
 class Employee(Person, Payable):
     def __init__(self, name, age, salary, credit_card):
         super().__init__(name, age, credit_card)
         self.salary = salary
 
-    def pay(self, amount):
-        self.credit_card.pay(amount)
+    def pey(self, amount):
+        self.credit_card.pey(amount)
 
-# Тестирование
 person = Person("Alice", 25, CreditCard(1000, 5000))
-student = Student("Bob", 20, "MIT", CreditCard(1000, 3000))
-employee = Employee("John", 35, 50000, CreditCard(2000, 10000))
+student = Student("Bob", 18, "MIT", CreditCard(1000, 3000))
+employee = Employee("John", 33, 50000, CreditCard(2000, 10000))
 
 print(student.introduce())
 print(employee.introduce())
 
-student.pay(200)
-employee.pay(200)
+student.pey(100)
+employee.pey(300)
